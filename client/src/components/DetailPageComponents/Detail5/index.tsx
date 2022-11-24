b73adcb02cc414a124d39afc94cc6f0d18a1f05c
Wincent33 <46631761+Wincent33@users.noreply.github.com>
2022-11-24 21:30:14 +0700

    fewcd

--- client/src/components/DetailPageComponents/Detail5/index.tsx
+++ client/src/components/DetailPageComponents/Detail5/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Slider from "@mui/material/Slider";
-import TextField from "@mui/material/TextField";
+import { GoInfo } from "react-icons/go";
 const Detail5 = () => {
   const percentage = [
     {
@@ -17,9 +17,7 @@ const Detail5 = () => {
   const handleOnChange = (event: any, newValue: any) => {
     setPersen(newValue);
   };
-  const textChange= (e:any)=>{setPersen(e.target.value)}
   const uangMuka = (persen / 100) * hargaProperti;
-  const uangMukaText= hargaProperti/uangMuka
   return (
     <div className="flex flex-row justify-between gap-4 w-[90vw] mx-auto max-w-[1200px] h-96">
       <div className="flex flex-col w-full bg-primary4 p-5">
@@ -28,7 +26,6 @@ const Detail5 = () => {
           Harga Properti: Rp {hargaProperti.toLocaleString()}
         </p>
         <p>Persentase Uang Muka: </p>
-      
         <div className="w-full mx-auto my-10">
           <Slider
             min={0}
@@ -43,12 +40,26 @@ const Detail5 = () => {
               <div className="text-base">{value}%</div>
             )}
           />
+          <div className="flex flex-row">
+            <GoInfo size={20} color={"black"} />
+            <label className="text-sm text-black">
+              Direkomendasikan Uang Muka Minimal 20% dari harga properti
+            </label>
+          </div>
           <p className="text-lg">
             Uang Muka(DP): Rp {uangMuka.toLocaleString()}
           </p>
         </div>
       </div>
-      <div className="flex w-full bg-primary4">right</div>
+      <div className="flex w-full bg-primary4">
+        <div>
+          <img
+            className="w-24 h-24 rounded-full"
+            src="https://images.unsplash.com/photo-1669178082499-341906b2ab28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=745&q=80"
+            alt="dew"
+          />
+        </div>
+      </div>
     </div>
   );
 };
