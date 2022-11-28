95540bf8ed445dfd9ba57645defe713390bb76a8
Wincent33 <46631761+Wincent33@users.noreply.github.com>
2022-11-28 10:57:11 +0700

    tgfrvv

--- client/src/components/SearchPageComponents/AdvancedFilter/FilterContent/Lokasi.tsx
+++ client/src/components/SearchPageComponents/AdvancedFilter/FilterContent/Lokasi.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-
-import "./style.scss";
 const SearchGIF = require("../../../../../assets/images/search.gif");
 var data = require("../../../../../Assets/Data/MOCK_DATA.json");
 
@@ -15,17 +13,21 @@ const Lokasi = (props: any) => {
   };
 
   return (
-    <div className="filter-lokasi-container">
-      <img className="search-gif" src={SearchGIF} alt="gif" />
+    // Filter Lokasi
+    <div className="flex justify-center w-full">
+      {/* Search GIF/Left */}
+      <img className="h-10 rounded-r-xl" src={SearchGIF} alt="gif" />
+      {/* Seach Input / Right */}
       <input
-        className="search-beli"
+        className="w-[60%] h-10 border-none outline-none p-0 pl-2 text-xl font-[600] text-primary5 capitalize"
         type="text"
         name="search-beli"
         placeholder="Lokasi..."
         value={locationSelect}
         onChange={onChange}
       />
-      <div className="search-result">
+      {/* Search Result */}
+      <div className="w-[433px] h-fit max-h-[200px] bg-white absolute translate-y-11 rounded-lg overflow-auto flex bg-clip-content flex-col z-50 scrollbar-hide">
         {data
           .filter((item: any) => {
             const searchTerm = locationSelect.toLowerCase();
@@ -38,7 +40,7 @@ const Lokasi = (props: any) => {
           .slice(0, 20)
           .map((item: any) => (
             <div
-              className="location"
+              className="bg-clip-content list-none p-4 m-0 hover:bg-primary5 hover:text-white hover:font-bold hover:cursor-pointer"
               key={item.id}
               onClick={() => {
                 onSearchList(item.place);
