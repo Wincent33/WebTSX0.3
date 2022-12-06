18874c02555443ec200357517b2e62549b11d346
Wincent33 <46631761+Wincent33@users.noreply.github.com>
2022-12-06 11:33:45 +0700

    rszdgv

---
+++ client/src/components/InputPageComponents/components/Developer.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import { TextField, MenuItem } from "@mui/material";
+import Select from "react-select";
+import DeveloperOptions from "../../../assets/data/DeveloperOptions.json";
+
+const Developer = (props) => {
+  const { formValue, setFormValue } = props;
+  const view =
+    formValue.developer === null && formValue.devLogo === null
+      ? [0]
+      : { label: formValue.developer, value: formValue.devLogo };
+
+  const handleOnChange = (e) => {
+    if (e !== null) {
+      let updatedValue = {};
+      updatedValue = { developer: e.label, devLogo: e.value };
+      setFormValue((prev) => ({ ...prev, ...updatedValue }));
+    } else {
+      let updatedValue = {};
+      updatedValue = { developer: null, devLogo: null };
+      setFormValue((prev) => ({ ...prev, ...updatedValue }));
+    }
+  };
+
+  return (
+    <div className="developer">
+      <label>Nama developer:</label>
+      <Select
+        isClearable={true}
+        isSearchable={true}
+        isMulti={false}
+        options={DeveloperOptions}
+        placeholder={"Kosongkan atau Pilih salah satu..."}
+        value={view}
+        onChange={handleOnChange}
+      />
+    </div>
+  );
+};
+
+export default Developer;
