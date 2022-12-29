1a83b347d5e0b4821f5d04ba34e7c0f9b9daa123
Wincent33 <46631761+Wincent33@users.noreply.github.com>
2022-12-29 22:05:56 +0700

    ghjk

--- client/src/pages/DashBoard/scenes/pesan/index.jsx
+++ client/src/pages/DashBoard/scenes/pesan/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { Box, Button, Checkbox, IconButton } from "@mui/material";
+import { Box, Button, Checkbox, IconButton, TextField } from "@mui/material";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 import { BiArchiveIn, BiTrash } from "react-icons/bi";
+import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 import { BsReply } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
 import Header from "../../components/Header";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import PrintIcon from "@mui/icons-material/Print";
+import ShareIcon from "@mui/icons-material/Share";
 const Pesan = () => {
   const Mail = () => {
     return (
@@ -39,6 +48,12 @@ const Pesan = () => {
       </Box>
     );
   };
+  const actions = [
+    { icon: <FileCopyIcon />, name: "Copy" },
+    { icon: <SaveIcon />, name: "Save" },
+    { icon: <PrintIcon />, name: "Print" },
+    { icon: <ShareIcon />, name: "Share" },
+  ];
   return (
     <Box className="m-5 flex flex-col">
       {/* Title */}
@@ -50,21 +65,67 @@ const Pesan = () => {
         gridAutoRows="140px"
         gap="20px"
       >
-          <Box className="overflow-y-auto py-5 col-span-5 row-span-4 bg-primary5 flex flex-col items-center rounded-3xl scrollbar-hide">
-            <Mail />
-            <Mail />
-            <Mail />
-            <Mail />
-            <Mail />
-            <Mail />
-            <Mail />
-            <Mail />
+        <Box className="overflow-y-auto py-5 col-span-4 row-span-5 bg-primary5 flex flex-col items-center rounded-3xl scrollbar-hide">
+          <Mail />
+          <Mail />
+          <Mail />
+          <Mail />
+          <Mail />
+          <Mail />
+          <Mail />
+          <Mail />
+        </Box>
+        <Box className=" justify-between overflow-y-auto col-span-8 row-span-5 bg-primary5 flex flex-col items-center rounded-3xl scrollbar-hide">
+          <Box className="flex flex-row p-4 bg-white w-full h-fit">
+            <Box className="flex h-fit my-auto w-[50%]">
+              <img
+                className="object-cover h-24 w-24 rounded-full"
+                src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                alt="123"
+              />
+              <Box className="flex flex-col justify-center gap-4 mx-4">
+                <h1 className="text-2xl text-primary5 font-bold">
+                  Adit Setiabudi
+                </h1>
+                <h1 className="text-primary5 flex flex-row text-lg">
+                  <GoPrimitiveDot color="gray" size="18px" />
+                  Offline
+                </h1>
+              </Box>
+            </Box>
+            <Box className="flex justify-end h-fit my-auto w-[50%]">
+              <IconButton>
+                <AiOutlineStar color="black" size="3rem" />
+              </IconButton>
+            </Box>
           </Box>
-          <Box className="overflow-y-auto py-5 col-span-5 row-span-4 bg-primary5 flex flex-col items-center rounded-3xl scrollbar-hide">
-            dsadas
+          <Box>test</Box>
+          <Box className="relative flex flex-row justify-center w-full h-28 bg-white">
+            <SpeedDial
+              ariaLabel="message-speeddial"
+              className="absolute bottom-6 left-6"
+              // sx={{ position: "absolute", bottom: 24, left: 24 }}
+              icon={<SpeedDialIcon />}
+            >
+              {actions.map((action) => (
+                <SpeedDialAction
+                  key={action.name}
+                  icon={action.icon}
+                  tooltipTitle={action.name}
+                />
+              ))}
+            </SpeedDial>
+            <TextField
+              id="text-message"
+              multiline
+              rows={4}
+              placeholder="ketik Pesan Anda"
+              className="w-[60%] h-full bg-primary4"
+            />
           </Box>
         </Box>
-        </Box>
+      </Box>
+    </Box>
   );
 };
 
