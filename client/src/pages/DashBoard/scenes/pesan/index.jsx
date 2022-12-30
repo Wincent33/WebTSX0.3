f928ca933b6f3913305e782e84cb936ce78f23eb
Wincent33 <46631761+Wincent33@users.noreply.github.com>
2022-12-30 21:15:15 +0700

    ghjkgh

--- client/src/pages/DashBoard/scenes/pesan/index.jsx
+++ client/src/pages/DashBoard/scenes/pesan/index.jsx
@@ -4,6 +4,7 @@ import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 import { BiArchiveIn, BiTrash } from "react-icons/bi";
 import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 import { BsReply } from "react-icons/bs";
+import { FiSend } from "react-icons/fi";
 import { GoPrimitiveDot } from "react-icons/go";
 import Header from "../../components/Header";
 import SpeedDial from "@mui/material/SpeedDial";
@@ -13,6 +14,18 @@ import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
 import SaveIcon from "@mui/icons-material/Save";
 import PrintIcon from "@mui/icons-material/Print";
 import ShareIcon from "@mui/icons-material/Share";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  Avatar,
+  MainContainer,
+  ConversationHeader,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  StarButton,
+  InfoButton,
+} from "@chatscope/chat-ui-kit-react";
 const Pesan = () => {
   const Mail = () => {
     return (
@@ -76,35 +89,85 @@ const Pesan = () => {
           <Mail />
         </Box>
         <Box className=" justify-between overflow-y-auto col-span-8 row-span-5 bg-primary5 flex flex-col items-center rounded-3xl scrollbar-hide">
-          <Box className="flex flex-row p-4 bg-white w-full h-fit">
-            <Box className="flex h-fit my-auto w-[50%]">
-              <img
-                className="object-cover h-24 w-24 rounded-full"
-                src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                alt="123"
-              />
-              <Box className="flex flex-col justify-center gap-4 mx-4">
-                <h1 className="text-2xl text-primary5 font-bold">
-                  Adit Setiabudi
-                </h1>
-                <h1 className="text-primary5 flex flex-row text-lg">
-                  <GoPrimitiveDot color="gray" size="18px" />
-                  Offline
-                </h1>
-              </Box>
-            </Box>
-            <Box className="flex justify-end h-fit my-auto w-[50%]">
-              <IconButton>
-                <AiOutlineStar color="black" size="3rem" />
-              </IconButton>
-            </Box>
+          <Box className="w-full h-full">
+            <MainContainer>
+              <ChatContainer>
+                <ConversationHeader className="h-24">
+                  <ConversationHeader.Content />
+                  <Avatar
+                    src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                    name="Adit"
+                    size="lg"
+                    status="invisible"
+                  />
+                  <ConversationHeader.Content
+                    className="ml-6"
+                    userName="Adit"
+                    info="Active 10 mins ago"
+                  />
+                  <ConversationHeader.Actions>
+                    <StarButton title="Add to favourites" />
+                    <InfoButton title="Show info" />
+                  </ConversationHeader.Actions>
+                </ConversationHeader>
+                <MessageList className="p-4">
+                  <Message
+                    model={{
+                      message: "Hello my friend",
+                      sentTime: "just now",
+                      direction: "incoming",
+                      sender: "Adit",
+                    }}
+                  />
+                  <Message
+                    model={{
+                      message: "Care to chat with me?",
+                      sentTime: "just now",
+                      direction: "incoming",
+                      sender: "Adit",
+                    }}
+                  />
+                  <Message
+                    model={{
+                      message: "Go Fuck Yourself, Adit!",
+                      sentTime: "just now",
+                      direction: "outgoing",
+                      sender: "me",
+                    }}
+                  />
+                  <Message
+                    model={{
+                      message: "How rude :((... i would if i could you bitch.",
+                      sentTime: "just now",
+                      direction: "ingoing",
+                      sender: "Adit",
+                    }}
+                  />
+                </MessageList>
+                <MessageInput
+                  className="mx-8"
+                  placeholder="Type message here"
+                />
+              </ChatContainer>
+            </MainContainer>
           </Box>
-          <Box>test</Box>
-          <Box className="relative flex flex-row justify-center w-full h-28 bg-white">
+          {/* <Box className="border-t-2 border-t-white border-t-solid relative flex flex-row justify-end w-full h-32 p-4 ">
             <SpeedDial
               ariaLabel="message-speeddial"
-              className="absolute bottom-6 left-6"
+              className="absolute bottom-[15%] left-[2.5%]"
               // sx={{ position: "absolute", bottom: 24, left: 24 }}
+              sx={{
+                "& .MuiFab-primary": {
+                  width: 90,
+                  height: 90,
+                  // flex: true,
+                  paddingBottom: 3,
+                  // textAlign: "center",
+                  "& .MuiSpeedDialIcon-icon": {
+                    fontSize: 50,
+                  },
+                },
+              }}
               icon={<SpeedDialIcon />}
             >
               {actions.map((action) => (
@@ -120,9 +183,14 @@ const Pesan = () => {
               multiline
               rows={4}
               placeholder="ketik Pesan Anda"
-              className="w-[60%] h-full bg-primary4"
+              className="w-[65%] h-full bg-primary5 rounded-t-3xl"
+              variant="standard"
             />
-          </Box>
+            <Box className="flex flex-row gap-4 text-xl w-[20%] border-solid border-[1px] border-white justify-center items-center text-white rounded-3xl m-4 duration-300 cursor-pointer hover:bg-primary4">
+              <FiSend size={40} color="white" />
+              Send
+            </Box>
+          </Box> */}
         </Box>
       </Box>
     </Box>
